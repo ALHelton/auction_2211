6221ba430fb307e401e02ae201db26e81c87aa3f
Andra Helton <andrahelton@gmail.com>
2023-01-08 10:45:20 -0700

    Add passing test for item_names

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -24,6 +24,7 @@ RSpec.describe Item do
       auction.add_item(item1)
       auction.add_item(item2)
       expect(auction.items).to eq([item1, item2])
+      expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
     end
   end
 
